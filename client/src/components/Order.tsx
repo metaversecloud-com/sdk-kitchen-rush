c90b43f7f639c976707fb0a7a53ffe11faef1d33
Selena Ramirez <selenaramirez@Selenas-MacBook-Air.local>
2026-03-26 15:49:23 -0400

    implemented order design, level one orders, and orderprops to use in order design

--- client/src/components/Order.tsx
+++ client/src/components/Order.tsx
@@ -1,1 +1,22 @@
 // renders order card visually
+// need icons for each ingredient
+// a timer progress bar
+// a visual distinction;
+import { OrderProps } from '../types/OrderProps'
+
+const Order = ({order, isActive}: OrderProps) => { 
+    return (
+      <div className={isActive ? 'active-order' : 'queued-order'}>
+      <span>{order.size}</span>
+      <span>{order.temp}</span>
+      <span>{order.milk}</span>
+      {order.flavor && <span>{order.flavor}</span>}
+      {order.toppings && order.toppings.map(topping => (
+        <span key={topping}>{topping}</span>
+      ))}
+      {isActive && <div className="timer">⏱</div>}
+    </div>
+    );
+};
+
+export default Order
---
+++ client/src/data/Coffee.ts
@@ -0,0 +1,14 @@
+import { Order } from "../types/Order"
+
+export const LEVEL_ONE_ORDERS: Order[] = [
+    { id: "1", size: "Small", temp: "Hot", milk: "Whole", timeLimit: 12000 },
+    { id: "2", size: "Medium", temp: "Iced", milk: "Oat", timeLimit: 12000 },
+    { id: "3", size: "Large", temp: "Hot", milk: "Almond", timeLimit: 12000 },
+    { id: "4", size: "Small", temp: "Iced", milk: "Whole", timeLimit: 12000 },
+    { id: "5", size: "Medium", temp: "Hot", milk: "None", timeLimit: 12000 },
+    { id: "6", size: "Large", temp: "Iced", milk: "Oat", timeLimit: 12000 },
+    { id: "7", size: "Small", temp: "Hot", milk: "Almond", timeLimit: 12000 },
+    { id: "8", size: "Medium", temp: "Iced", milk: "None", timeLimit: 12000 },
+    { id: "9", size: "Large", temp: "Hot", milk: "Whole", timeLimit: 12000 },
+    { id: "10", size: "Small", temp: "Iced", milk: "Almond", timeLimit: 12000 },
+]
